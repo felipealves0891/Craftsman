548888b6b48af06d97c53bf239794317a960c90b
Felipe Alves <felipealves0891@gmail.com>
2026-04-25 16:03:31 -0300

    Adicionado codigo para efetuar as migrações de forma automatica em dev

--- src/App/Program.cs
+++ src/App/Program.cs
@@ -2,6 +2,7 @@ using Craftsman.App.Services;
 using Craftsman.Infra.Persistence;
 using Craftsman.Infra.Services;
 using Microsoft.AspNetCore.DataProtection;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +18,15 @@ builder.Services.AddCraftsmanInfrastructure(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    using var scope = app.Services.CreateScope();
+    using var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>(); 
+    dbContext.Database.EnsureCreated();
+    dbContext.Database.Migrate();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
